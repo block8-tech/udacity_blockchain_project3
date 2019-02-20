33f0079bafc408f8789fffcb0e19945f46eefefe
Block8-tech <block8tech@gmail.com>
2019-02-20 11:02:08 +0000

    getBlockByIndex() working

--- BlockController.js
+++ BlockController.js
@@ -23,38 +23,53 @@ class BlockController {
 
 
     /**
-     * GET Endpoint to retrieve a block by index (height)"
+     * GET Endpoint to retrieve a block by index (height)
+     * returns a JSON string
+     * Success => returns the block
+     * Failure => returns error message
      */
     async getBlockByIndex() {
         //1. Check blockchain height
-        //2. Use height in a conditional block
-        //3. if within height then retrieve block
-        //4. if not, return error message.
+        //2. Create the endpoint
+        //3. Store the request parameter (:index)
+        //4. Check the :index value is not greater than the chain height
+        //5. Check for incompatible :index request and send error message back
+        //6. Deal with any other HTTP request errors
+
 
         //1.
         const chainHeight = await this.db.getBlockHeight();
 
+        //2.
         this.app.get("/block/:index", async (req, res) => {
 
-            //2. Get the index value from the req.params.index
-            const indx = req.params.index;
+            //store the request url - use in error handling.
+            const _req = req.headers.host + req._parsedUrl.pathname;
 
             //3.
+            const indx = req.params.index;
+
+            //4.
+            if(indx <= chainHeight) {
+                const block = await this.db.getBlock(indx).catch(console.log);
+                res.send(block);
+                res.end();
+            }
+
+            //5.
             if(indx > chainHeight){
-                // The HTTP request was for a block that does not exist. ERROR
                 const errorMessage = {
                     error: `Block does not exist.`,
                     message: `The blockchain length = ${chainHeight} at the time of your request. You requested block number ${indx} which exceeds the chain length at the time of your request.`
                 };
                 res.send(JSON.stringify(errorMessage));
                 res.end();
-
-            } else if(indx <= chainHeight) {
-                const block = await this.db.getBlock(indx).catch(console.log);
-                res.send(block);
+            } else {
+                //6.
+                res.send(`ERROR: error with your HTTP GET request: ${_req}`);
                 res.end();
+                throw new Error(`ERROR: error with your HTTP GET request: ${_req}`);
             }
-
         });
     }
 
